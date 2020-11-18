df6d354bdf60d01dfddfcbeb1e710be573b33cdb
User <User@LAPTOP-UTFRNLVT>
2020-11-18 12:44:17 +0700

    perubahan database

--- src/Controller/DatabaseHandler.java
+++ src/Controller/DatabaseHandler.java
@@ -17,8 +17,8 @@ import javax.swing.JOptionPane;
 public class DatabaseHandler {
     public Connection con;
     private String driver = "com.mysql.cj.jdbc.Driver";
-    //private String url = "jdbc:mysql://localhost/db_tubes_pbo";
-    private String url = "jdbc:mysql://localhost/db_tubes_pbo?serverTimezone=" + TimeZone.getDefault().getID();
+    //private String url = "jdbc:mysql://localhost/kuis_pbo";
+    private String url = "jdbc:mysql://localhost/kuis_pbo?serverTimezone=" + TimeZone.getDefault().getID();
     private String username = "root";
     private String password = "";
 
