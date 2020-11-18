172b22a7fc89a3ce58c782cd4753ba045fa8e370
User <User@LAPTOP-UTFRNLVT>
2020-11-18 09:46:37 +0700

    create project kuisPBO

---
+++ src/Main/Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Main;
+
+import Controller.Controller;
+
+/**
+ *
+ * @author User
+ */
+public class Main {
+
+    static Controller controller = new Controller();
+    
+    public static void main(String[] args) {
+        
+
+    }
+    
+}
